d4442e4cc80e4d228601828022fdf18cc0d24c6e
banelmikhael <banel.mikhael.bm@gmail.com>
2022-11-17 19:28:16 +0300

    assigment completed

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
   //add static methods here
+  static capitalize(arr) {
+    return `${arr[0].toUpperCase()}${arr.slice(1)}`
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '')
+  }
+  static titleize(string) {
+    const splitString = string.split(' ')
+    let newArray = []
+    const excludeWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    for (const word of splitString) {
+      newArray.push(excludeWords.includes(word) ? word :cap(word))
+    }
+    
+    return cap(newArray.join(" "))
+  }
+}
+
+function cap (string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
 }
